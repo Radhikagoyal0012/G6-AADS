95fcde646118c629b9c586176d59ed5da4437f52
Radhika <radhikagoyal298@gmail.com>
2026-07-13 11:42:01 +0530

    remove nth node from end

---
+++ src/removeNthNodeFromEnd.java
@@ -0,0 +1,57 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Solution2{
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+    public void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution2 sol = new Solution2();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        sol.printList(head);
+
+        head = sol.removeNthFromEnd(head, 2);
+
+        System.out.print("After Removing 2nd Node from End: ");
+        sol.printList(head);
+    }
+}
